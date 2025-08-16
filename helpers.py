a89a779679cc87982f01e79cc4dd30199197900d
Kevin Joe <jlk.zhou@gmail.com>
2025-08-16 11:34:18 +0800

    Improved structuring of functions

--- helpers.py
+++ helpers.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class Quake: 
@@ -30,24 +31,55 @@ def get_page(url):
     return driver
 
 
-def get_quake_table(driver, table_class): 
+def get_table_body(driver, table_class): 
+    '''Returns a list of rows or <tr> tags that contain the earthquake data. 
+    The first element of that list is the row of <th> headings of the table. '''
     wait = WebDriverWait(driver, 20)
     wait.until(lambda driver: 
                len(driver.find_elements(
                    By.XPATH, f"//table[@id='{table_class}']/tbody/tr")) >= 10)
+    
     rows = driver.find_elements(By.XPATH, f"//table[@id='{table_class}']/tbody/tr")
     return rows
 
 
-def add_coordinates(rows, table_class): 
-    for row in rows: 
+def get_table_header(driver, table_class): 
+    '''Returns a list of table headers given a table in a page. '''
+    wait = WebDriverWait(driver, 20)
+    wait.until(lambda driver: 
+               len(driver.find_elements(
+                   By.XPATH, f"//table[@id='{table_class}']/tbody/tr/th")) >= 5
+    )
+    headers = driver.find_elements(By.XPATH, 
+                                   f"//table[@id='{table_class}']/tbody/tr/th")
+    for i in range(len(headers)): 
+        headers[i] = headers[i].text 
+    return headers
+
+
+def get_data_from_row(row):
+    '''Returns a list of earthquake data, given one <tr> table row. '''
+    if row: 
         cells = row.find_elements(By.TAG_NAME, "td")
         if cells: 
-            detail_url = cells[0].find_element(By.TAG_NAME, "a").get_attribute("href")
-            detail_page = get_page(detail_url)
-            detail_table = get_quake_table(detail_page, table_class)
-            if detail_table: 
-                latitude = detail_table[1]
+            for i in range(len(cells)): 
+                cells[i] = cells[i].text
+        return cells
+    
+
+def get_link_from_row(row):
+    pass
+
+
+# def add_coordinates(rows, table_class): 
+#     for row in rows: 
+#         cells = row.find_elements(By.TAG_NAME, "td")
+#         if cells: 
+#             detail_url = cells[0].find_element(By.TAG_NAME, "a").get_attribute("href")
+#             detail_page = get_page(detail_url)
+#             detail_table = get_table(detail_page, table_class)
+#             if detail_table: 
+#                 latitude = detail_table[1]
     
 
 def parser(rows): 
--- test.py
+++ test.py
@@ -6,5 +6,10 @@ site_data = [
     ]
 
 driver = get_page(site_data[1])
-table = get_quake_table(driver, site_data[2])
-add_coordinates(table)
+header = get_table_header(driver, site_data[2])
+table = get_table_body(driver, site_data[2])
+table.pop(0)
+
+print(header)
+for row in table: 
+    print(get_data_from_row(row))
