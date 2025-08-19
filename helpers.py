6aa9c7f6b1c5efabc7d8e3b1cddb40421b8c5121
Kevin Joe <jlk.zhou@gmail.com>
2025-08-19 17:24:13 +0800

    Added coordinate information to crawled results

--- helpers.py
+++ helpers.py
@@ -3,107 +3,97 @@ from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 
 
-class Quake: 
-    '''Data structure for each earthquake occurrence. Includes the date of
-    the earthquake, location, and magnitude. '''
-    def __init__(self, date, location, magnitude):
+class Quake:
+    """Data structure for each earthquake occurrence. Includes the date of
+    the earthquake, location, and magnitude."""
+
+    def __init__(
+        self, date=None, location=None, magnitude=None, longitude=None, latitude=None
+    ):
         self.date = date
         self.location = location
         self.magnitude = magnitude
+        self.longitude = longitude
+        self.latitude = latitude
 
-    def __str__(self): 
-        return f"Magnitude {self.magnitude} Earthquake at {self.location}, {self.date}"
+    def __str__(self):
+        return f"""Magnitude {self.magnitude} Earthquake at {self.location}, {self.date}, longitude {self.longitude}, latitude {self.latitude}"""
 
 
-def get_page(url): 
+def get_page(url):
+    """Returns a Selenium Chrome webdriver object given an url."""
     options = Options()
     options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=options)
 
-    try: 
+    try:
         driver.get(url)
-    except WebDriverException: 
+    except WebDriverException:
         return None
-    
+
     return driver
 
 
-def get_table_body(driver, table_class, wait_rows): 
-    '''Returns a list of rows or <tr> tags that contain the earthquake data. 
-    The first element of that list is the row of <th> headings of the table. 
-    Please also specify how many rows you would like the driver to wait for
-    loading using wait_rows. '''
+def get_table(driver, table_class, wait_rows):
+    """Returns a list of rows or <tr> tags that contain the earthquake data.
+    The first element of that list is the row of <th> headings of the table.
+    Please also specify how many total table rows, header row included,
+    you would like the driver to wait for loading using wait_rows."""
     wait = WebDriverWait(driver, 20)
 
-    wait.until(lambda d: 
-                len(d.find_elements(
-                   By.XPATH, f"//table[@id='{table_class}']/tbody/tr")) >= wait_rows and 
-                len(d.find_elements(
-                   By.XPATH, f"//table[@id='{table_class}']/tbody/tr/th")) >= 5
+    wait.until(
+        lambda d: len(
+            d.find_elements(By.XPATH, f"//table[@id='{table_class}']/tbody/tr")
+        )
+        >= wait_rows
+        and len(d.find_elements(By.XPATH, f"//table[@id='{table_class}']/tbody/tr/th"))
+        >= 5
     )
-    # headers = driver.find_elements(By.XPATH, 
-    #                                f"//table[@id='{table_class}']/tbody/tr/th")
-    # if headers:
-    #     for i in range(len(headers)): 
-    #         headers[i] = headers[i].text 
-    
+
     rows = driver.find_elements(By.XPATH, f"//table[@id='{table_class}']/tbody/tr")
     return rows
 
 
-# def get_table_header(driver, table_class): 
-#     '''Returns a list of table headers given a table in a page. '''
-#     wait = WebDriverWait(driver, 20)
-#     wait.until(lambda driver: 
-               
-#     )
-    
-    
-#     return headers
-
-
 def get_data_from_row(row):
-    '''Returns a list of earthquake data, given one <tr> table row. '''
-    if row: 
+    """Returns a list of headers or earthquake data, given one <tr> table row."""
+    if row:
         cells = row.find_elements(By.XPATH, ".//td | .//th")
-        if cells: 
-            for i in range(len(cells)): 
+        if cells:
+            for i in range(len(cells)):
                 cells[i] = cells[i].text
         return cells
-    
+
 
 def get_link_from_row(row):
-    '''Returns the link in a row, given the row has one. '''
+    """Returns the link in a row, given the row has one."""
+    # TODO: What if a row doesn't have a link? Add defensive features.
     a = row.find_element(By.TAG_NAME, "a")
     return a.get_attribute("href")
 
 
-# def add_coordinates(rows, table_class): 
-#     for row in rows: 
-#         cells = row.find_elements(By.TAG_NAME, "td")
-#         if cells: 
-#             detail_url = cells[0].find_element(By.TAG_NAME, "a").get_attribute("href")
-#             detail_page = get_page(detail_url)
-#             detail_table = get_table(detail_page, table_class)
-#             if detail_table: 
-#                 latitude = detail_table[1]
-    
+def parser(site_data):
+    """Parse the earthquake site given its site data."""
+    driver = get_page(site_data[1])
+    table = get_table(driver, site_data[2], 20)
 
-def parser(rows): 
     quakes = []
-    for row in rows: 
-        cells = row.find_elements(By.TAG_NAME, "td")
-        if cells: 
-            quake = Quake(
-                cells[0].find_element(By.TAG_NAME, "a").text, 
-                cells[1].text, 
-                cells[2].text
-            )
-            quakes.append(quake)
 
-    for quake in quakes: 
+    for row in table[1:]:
+        quake = Quake()
+        basic_data = get_data_from_row(row)
+        quake.date = basic_data[0]
+        quake.location = basic_data[1]
+        quake.magnitude = basic_data[2]
+
+        link = get_link_from_row(row)
+        detailed_page = get_page(link)
+        detailed_table = get_table(detailed_page, site_data[2], 2)
+        detailed_data = get_data_from_row(detailed_table[1])
+        quake.latitude = detailed_data[1]
+        quake.longitude = detailed_data[2]
+        quakes.append(quake)
         print(quake)
 
+    return quakes
--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 from helpers import *
 
-def main(): 
+
+def main():
 
     # Relevant information about JMA earthquake site
     site_data = [
-        "JMA", "https://www.data.jma.go.jp/multi/quake/index.html?lang=en", 
-        "quakeindex_table"
+        "JMA",
+        "https://www.data.jma.go.jp/multi/quake/index.html?lang=en",
+        "quakeindex_table",
     ]
 
-    # Get page from site data
-    driver = get_page(site_data[1])
+    parser(site_data)
 
-    # Parse information from site into readable format, and print them
-    table = get_quake_table(driver, site_data[2])
 
-    parser(table)
-
-if __name__ == "__main__": 
-    main() 
+if __name__ == "__main__":
+    main()
